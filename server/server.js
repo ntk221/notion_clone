78bb47a182f1df11b0dde77e6e847f8c7753b385
ntk221 <nittakdub@gmail.com>
2023-03-22 22:45:14 +0900

    [#8] 認証機能のためにデータモデリングを修正した
    [#10] mongoose を 使って，MongoDB Atlasに接続する
    
    [#10] dotenv を使って，データベースの情報のハードコーディングを無くした

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const helmet = require("helmet");
+const mongoose = require("mongoose");
 const app = express();
-
 app.use(helmet());
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("success!");
+})
+.catch((error) => {
+  console.error("Error!");
+})
 
 // GET '/' 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).send("hello\n");
+})
+app.post("/signup", (req, res) => {
+  /* ... */
 })
 
 app.listen(3000, ()=> {
