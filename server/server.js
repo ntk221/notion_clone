c2bce57781142c0d04b7565b83523982defb678c
ntk221 <nittakdub@gmail.com>
2023-03-23 13:42:11 +0900

    [#10] mongoose を 使って，MongoDB Atlasに接続する

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const helmet = require("helmet");
+const mongoose = require("mongoose");
 const app = express();
 app.use(helmet());
+
+mongoose.connect("mongodb+srv://Kazuki:vzo60AmntFz7h24M@cluster0.aydmxll.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("success!");
+})
+.catch((error) => {
+  console.error("Error!");
+})
 
 // GET '/' 
 app.get("/", (req, res) => {
