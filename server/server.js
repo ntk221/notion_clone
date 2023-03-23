2c19147ff117052bb3f3cf7a3e525d9b30a9df3b
ntk221 <nittakdub@gmail.com>
2023-03-23 16:14:47 +0900

    [#14] ユーザー新規登録のためのエンドポイントを作成した

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(helmet());
 require("dotenv").config();
@@ -27,14 +29,26 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
 });
+
 const User = mongoose.model("User", UserSchema);
 
 // GET '/' 
 app.get("/", (req, res) => {
     res.status(200).send("hello\n");
 })
-app.post("/signup", (req, res) => {
-  /* ... */
+app.post("/signup", async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 5);
+  const user = new User({
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  try {
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 })
 
 app.listen(3000, ()=> {
