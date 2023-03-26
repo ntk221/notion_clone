3287bd8a07aa25bf9d0d58b48fa03be3b7ba4f7d
ntk221 <nittakdub@gmail.com>
2023-03-26 15:20:10 +0900

    [#38] Articleモデルを実装

---
+++ server/models/Article.js
@@ -0,0 +1,11 @@
+const articleSchema = new mongoose.Schema({
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    body: { type: String, required: true},
+    title: { type: String, required: true},
+    createdAt: { type: Date, default: Date.now},
+    updatedAt: { type: Date, default: Date.now},
+  });
+  
+  const Article = mongoose.model("Article", articleSchema);
+
+  module.exports = Article;
