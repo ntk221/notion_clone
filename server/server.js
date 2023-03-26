dae8639e3da899417898d3740e88c1eeeac90602
ntk221 <nittakdub@gmail.com>
2023-03-26 15:11:46 +0900

    [#36] 包括的エラーハンドリングを設定，データベースの接続エラーについても対応

--- server/server.js
+++ server/server.js
@@ -1,64 +1,58 @@
-const express = require("express");
+const { express, json } = require("express");
 const helmet = require("helmet");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const app = express();
 const mongoose = require("mongoose");
-const { json } = require("express");
+require("dotenv").config();
+
+const app = express();
 app.use(helmet());
 app.use(json());
-require("dotenv").config();
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  }
-});
-
-const User = mongoose.model("User", UserSchema);
-
-// GET '/' 
-app.get("/", (req, res) => {
-    res.status(200).send("hello\n");
 })
+.then(() => {
+  app.listen(8001, () => {
+    console.log("start listening");
+  });
+})
+.catch((err) => {
+  console.error("MongoDB connection err:", err);
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+  });
+});
+
+
+// GET '/'
+app.get("/", (req, res) => {
+  res.status(200).send("hello\n");
+});
 
 // 新規ユーザー登録
-app.post("/signup", async (req, res) => {
-  console.log(req.body);
-  console.log(req.body.password);
+app.post("/signup", async (req, res, next) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 5);
   const user = new User({
     email: req.body.email,
     password: hashedPassword,
-    username: req.body.username
+    username: req.body.username,
   });
   try {
     await user.save();
-    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
+    const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
     res.status(201).json({ token });
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    next(err);
   }
-})
+});
 
-app.listen(8001, ()=> {
-    console.log("start listening");
-})
+// 包括的エラーハンドリング
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+});
 
 module.exports = app;
