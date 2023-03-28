78bf2a6ef6dc38a95dc33b91a4b6dd798874a7ab
ntk221 <nittakdub@gmail.com>
2023-03-28 18:02:27 +0900

    [#47] サーバー側で発行したJWTトークンをブラウザのローカルサーバーに保存するロジックを追加

--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -1,6 +1,15 @@
-import React, { useState, FormEvent} from "react";
+import React, { useState, FormEvent } from "react";
 import axios, { formToJSON } from "axios";
-import { Box, Button, FormControl, FormLabel, Input, VStack, Text, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Text,
+  Link,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 const SignupForm: React.FC = () => {
@@ -24,9 +33,11 @@ const SignupForm: React.FC = () => {
     navigate("/login");
   };
 
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const form = event.currentTarget.form;
+    const form = event.currentTarget;
     if (!form) return;
     const formData = new FormData(form);
     const jsonData = formToJSON(formData);
@@ -34,27 +45,55 @@ const SignupForm: React.FC = () => {
       const response = await axios.post("/signup", jsonData, {
         headers: { "Content-type": "application/json" },
       });
+      const token = response.data.token;
+      localStorage.setItem("token", token);
       console.log(response);
+      navigate("/home");
     } catch (error) {
-      console.error(error);
+      if (axios.isAxiosError(error)) {
+        console.error(error.response?.data.error);
+        setErrorMessage(error.response?.data.error);
+      } else {
+        console.log(error);
+      }
     }
   };
 
   return (
     <Box>
+      {errorMessage && (
+        <Text color="red" mb="4">
+          {errorMessage}
+        </Text>
+      )}
       <form onSubmit={handleSubmit}>
         <VStack spacing="4">
           <FormControl id="username">
             <FormLabel>ユーザー名</FormLabel>
-            <Input type="text" name="username" value={formData.username} onChange={handleInputChange} />
+            <Input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+            />
           </FormControl>
           <FormControl id="email">
             <FormLabel>Eメール</FormLabel>
-            <Input type="email" name="email"value={formData.email} onChange={handleInputChange} />
+            <Input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
           </FormControl>
           <FormControl id="password">
             <FormLabel>パスワード</FormLabel>
-            <Input type="password" name="password" value={formData.password} onChange={handleInputChange} />
+            <Input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
           </FormControl>
           <Button type="submit" colorScheme="blue" w="full">
             登録する
@@ -62,7 +101,10 @@ const SignupForm: React.FC = () => {
         </VStack>
       </form>
       <Text mt="4">
-        アカウントをお持ちですか？ <Link href="/login">ログイン</Link>
+        アカウントをお持ちですか？{" "}
+        <Link href="/login" color="blue">
+          ログイン
+        </Link>
       </Text>
     </Box>
   );
