0dca8221786d179ee134aa6fff3fbe82173b0c07
ntk221 <nittakdub@gmail.com>
2023-03-31 17:58:15 +0900

    [#55] DashBoard コンポーネントで，user情報を取得するエンドポイントを叩いて子コンポーネントに渡すように実装

--- frontend/src/components/Pages/DashBoard.tsx
+++ frontend/src/components/Pages/DashBoard.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import SideBar from '../SideBar'
+import axios from "axios";
+
 
 const DashBoard = () => {
+  const [userData, setUserData] = useState("");
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await axios.get("/user", {
+          headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        }},);
+        setUserData(response.data.username);
+        console.log(userData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUserData();
+  }, []);
+
   return (
-      <SideBar username='knitta' notes={["記事1", "記事２", "記事３"]} onNoteClick={(note: string) => {console.log(note)}}/>
+      <SideBar username={ userData } notes={["記事1", "記事２", "記事３"]} onNoteClick={(note: string) => {console.log(note)}}/>
   )
 }
 
