71377e569c24f6024775e2e28ab91b805c0d4395
ntk221 <nittakdub@gmail.com>
2023-03-31 18:27:45 +0900

    [#57] DashBoard コンポーネントで，localsorageのJWTトークンを確認してリダイレクトさせる処理をuseEffectで追加した

--- frontend/src/components/Pages/DashBoard.tsx
+++ frontend/src/components/Pages/DashBoard.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import SideBar from '../SideBar'
 import axios from "axios";
 
@@ -20,7 +21,15 @@ const DashBoard = () => {
       }
     };
     getUserData();
-  }, []);
+  });
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/");
+    }
+  });
 
   return (
       <SideBar username={ userData } notes={["記事1", "記事２", "記事３"]} onNoteClick={(note: string) => {console.log(note)}}/>
