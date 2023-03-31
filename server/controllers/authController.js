89fd0392c4459d538f5686776fbeb0623f6362ed
ntk221 <nittakdub@gmail.com>
2023-03-31 17:00:31 +0900

    [#53] /user エンドポイントを実装

---
+++ server/controllers/authController.js
@@ -0,0 +1,30 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+exports.signup = async (req, res, next) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 5);
+    const user = new User({
+      email: req.body.email,
+      password: hashedPassword,
+      username: req.body.username,
+    });
+    try {
+      await user.save();
+      const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
+      res.status(201).json({ token });
+    } catch (err) {
+      if (err.name === "MongoServerError" && err.code === 11000) {
+        if (err.keyPattern.email) {
+          return res.status(409).json({ error: "Email already exists" });
+        } else if (err.keyPattern.username) {
+          return res.status(409).json({ error: "Username already exists" });
+        }
+      }
+      next(err);
+    }
+  };
+
+exports.checkAuth = (req, res) => {
+    res.status(200).json({ message: "Authentication succeeded" });
+  };
