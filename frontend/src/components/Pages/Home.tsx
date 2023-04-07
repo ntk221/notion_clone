8117af229283463a5d7427455b98f75a843fcfde
ntk221 <nittakdub@gmail.com>
2023-04-07 20:31:14 +0900

    [#70] check

---
+++ frontend/src/components/Pages/Home.tsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+import { Box, Button } from "@chakra-ui/react";
+import { useNavigate } from 'react-router-dom';
+import { ArticleContext } from '../Layout/AppLayout';
+import axios from "axios";
+import { UserContext } from '../Layout/AppLayout';
+
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+  const user  = useContext(UserContext);
+  const { setSelectedArticle } = useContext(ArticleContext);
+
+  const handleCreateArticle = async () => {
+    try {
+      const response = await axios.post("/articles", {
+        title: "無題",
+        body: "ここに記事の内容を書いてください",
+        author: user.id,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Contyent-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      console.log(response.data);
+      setSelectedArticle(response.data);
+      navigate(`/dashboard/article/${response.data._id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return ( 
+    <Box display="flex" alignItems="center" justifyContent="center" w="100%">
+      <Button onClick={handleCreateArticle}>新規記事を作成</Button>
+    </Box>
+  )
+}
+
+export default Home;
