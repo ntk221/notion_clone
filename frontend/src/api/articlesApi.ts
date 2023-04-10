0716e59dc413354027a1d334c78039d18b1bf400
ntk221 <nittakdub@gmail.com>
2023-04-10 20:07:36 +0900

    [#75] 更新処理として，バックエンドに，postメソッドを送るように実装

--- frontend/src/api/articlesApi.ts
+++ frontend/src/api/articlesApi.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { IArticle } from "../types/articleType";
 
 const fetchArticles = async (userId : string) => {
     try {
@@ -12,6 +13,27 @@ const fetchArticles = async (userId : string) => {
       });
       console.log(response);
       return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+export const postArticle = async(editTitle : string, editBody : string, selectedArticle : IArticle) => {
+  try {
+        const response = await axios.post(`/articles`, {
+        _id : selectedArticle._id,
+        title: editTitle,
+        body: editBody,
+        author: selectedArticle.author,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      return response;
     } catch (error) {
       console.log(error);
     }
