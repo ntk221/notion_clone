98609385483f8700c05aa866919114a3736cc69f
ntk221 <nittakdub@gmail.com>
2023-04-12 18:56:22 +0900

    [#83] サイドバーの追加ボタンで，記事の追加ができるように実装

--- frontend/src/components/Pages/Home.tsx
+++ frontend/src/components/Pages/Home.tsx
@@ -2,38 +2,25 @@ import React, { useContext } from "react";
 import { Box, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { ArticleContext, UserContext } from "../../contexts/contexts";
-import axios from "axios";
+import { createArticle } from "../../api/articlesApi";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
   const user = useContext(UserContext);
   const { setSelectedArticle, setUserArticles } = useContext(ArticleContext);
 
+
   const handleCreateArticle = async () => {
     try {
-      const response = await axios.post(
-        "/articles",
-        {
-          title: "",
-          body: "",
-          author: user.id,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-            "Contyent-Type": "application/json",
-            Accept: "application/json",
-          },
-        }
-      );
+      const response = await createArticle("","", user.id);
 
-      await setSelectedArticle(response.data);
+      await setSelectedArticle(response);
 
       const addedArticle = response;
 
-      await setUserArticles((prev) => [...prev, addedArticle.data]);
+      await setUserArticles((prev) => [...prev, addedArticle]);
 
-      navigate(`/dashboard/article/${response.data._id}`);
+      navigate(`/dashboard/article/${response._id}`);
     } catch (error) {
       console.log(error);
     }
