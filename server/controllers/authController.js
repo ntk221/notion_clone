38e20ea06f3bb077a21bf96ffaaa75792b50c005
ntk221 <nittakdub@gmail.com>
2023-04-13 20:37:05 +0900

    [#62] バリデーションチェックを実装

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,7 +6,7 @@ exports.signup = async (req, res, next, UserModel = User) => {
   try {
     const existingUser = await UserModel.findOne({ $or: [{ email: req.body.email }, { username: req.body.username }] });
     if (existingUser) {
-      return res.status(409).json({ error: existingUser.email === req.body.email ? "Email already exists" : "Username already exists" });
+      return res.status(409).json({ error: existingUser.email === req.body.email ? "メールアドレスはすでに登録されています" : "ユーザー名はすでに登録されています" });
     }
     const hashedPassword = await bcrypt.hash(req.body.password, 5);
     const user = new UserModel({
@@ -26,11 +26,11 @@ exports.login = async (req, res, next, UserModel = User) => {
   try {
     const user = await UserModel.findOne({ email: req.body.email });
     if (!user) {
-      return res.status(401).json({ error: "Auth failed" });
+      return res.status(401).json({ error: "ユーザー情報が登録されていません" });
     }
     const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ error: "Auth failed" });
+      return res.status(401).json({ error: "パスワードが違います" });
     }
     const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
     res.status(200).json({ token });
@@ -43,7 +43,3 @@ exports.checkAuth = (req, res) => {
   res.status(200).json({ message: "Authentication succeeded" });
 };
 
-
-exports.checkAuth = (req, res) => {
-    res.status(200).json({ message: "Authentication succeeded" });
-  };
