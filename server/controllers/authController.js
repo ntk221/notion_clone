e9d301355593e33b4b51f27e7c587f37ffff4dec
ntk221 <nittakdub@gmail.com>
2023-04-13 19:49:43 +0900

    [#85] ログイン機能を実装

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,27 +3,46 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 exports.signup = async (req, res, next, UserModel = User) => {
+  try {
+    const existingUser = await UserModel.findOne({ $or: [{ email: req.body.email }, { username: req.body.username }] });
+    if (existingUser) {
+      return res.status(409).json({ error: existingUser.email === req.body.email ? "Email already exists" : "Username already exists" });
+    }
     const hashedPassword = await bcrypt.hash(req.body.password, 5);
     const user = new UserModel({
       email: req.body.email,
       password: hashedPassword,
       username: req.body.username,
     });
-    try {
-      const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
-      res.status(201).json({ token });
-    } catch (err) {
-      // ユーザー名またはメールアドレスが既に存在する場合
-      if (err.name === "MongoServerError" && err.code === 11000) {
-        if (err.keyPattern.email) {
-          return res.status(409).json({ error: "Email already exists" });
-        } else if (err.keyPattern.username) {
-          return res.status(409).json({ error: "Username already exists" });
-        }
-      }
-      next(err);
+    const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
+    await user.save();
+    res.status(201).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next, UserModel = User) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).json({ error: "Auth failed" });
     }
-  };
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Auth failed" });
+    }
+    const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
+    res.status(200).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.checkAuth = (req, res) => {
+  res.status(200).json({ message: "Authentication succeeded" });
+};
+
 
 exports.checkAuth = (req, res) => {
     res.status(200).json({ message: "Authentication succeeded" });
